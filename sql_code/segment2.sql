dde2fbe968a79a58d6bcdf7dd886d0759a2f8ee6
colombianguate <50925704+colombianguate@users.noreply.github.com>
2021-02-22 21:58:15 -0800

    starting join sql

--- sql_code/segment2.sql
+++ sql_code/segment2.sql
@@ -2,7 +2,7 @@
 CREATE TABLE Vaccines (
     country VARCHAR(40) NOT NULL,
     iso_code VARCHAR(40) NOT NULL,
-    date DATE NOT NULL,
+    dates DATE NOT NULL,
     total_vaccines INT NOT NULL,
     daily_vaccinations INT NOT NULL,
     total_people_vaccindated_per_hundred INT NOT NULL,
@@ -16,5 +16,19 @@ CREATE TABLE Vaccines (
 
 
 -- Creating table for USA Data
+CREATE TABLE USA_Data(
+    dates DATE NOT NULL,
+
+    FOREIGN KEY(date) REFERENCES Vaccines(date)
+);
 
 
+
+-- Joining tables
+
+SELECT v.dates
+INTO merged_dataset
+FROM Vaccines AS v
+FULL OUTER JOIN USA_Data as usa
+ON v.datse = usa.dates
+ORDER BY v.dates
