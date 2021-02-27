3dd47d869d8c441c2ad61fd1b99f58fb8fc26797
colombianguate <50925704+colombianguate@users.noreply.github.com>
2021-02-27 12:01:39 -0800

    finishing databse

--- sql_code/segment2.sql
+++ sql_code/segment2.sql
@@ -5,8 +5,8 @@ CREATE TABLE Vaccines (
     people_vaccinated INT NOT NULL,
     people_fully_vaccinated INT NOT NULL,
     daily_vaccinations INT NOT NULL,
-    total_people_vaccindated_per_hundred INT NOT NULL,
-    people_vacinated_per_hundred INT NOT NULL,
+    total_vaccinations_per_hundred INT NOT NULL,
+    people_vaccinated_per_hundred INT NOT NULL,
     people_fully_vaccinated_per_hundred INT NOT NULL,
     PRIMARY KEY (country)
 );
@@ -19,7 +19,6 @@ CREATE TABLE Cases(
     new_deaths INT NOT NULL,
     total_cases INT NOT NULL,
     total_deaths INT NOT NULL,
-
     FOREIGN KEY(country) REFERENCES Vaccines(country)
 );
 
