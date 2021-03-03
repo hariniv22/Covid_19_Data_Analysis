d51ea8d6e1309bd5e87cdbafb25b3ebbef90914a
colombianguate <50925704+colombianguate@users.noreply.github.com>
2021-03-02 19:48:37 -0800

    databse sql codE

--- sql_code/segment3.sql
+++ sql_code/segment3.sql
@@ -1,16 +1,23 @@
 -- Create table for COVID-19 Dataset
 
-CREATE covid_19_data (
+CREATE TABLE covid_19_data (
     cdc_case_earliest_dt DATE IS NOT NULL,
     cdc_report_dt DATE IS NOT NULL,
     pos_spec_dt DATE IS NOT NULL,
     onset_dt DATE IS NOT NULL,
-    current_status DATE IS NOT NULL,
+    current_status VARCHAR(40),
     sex VARCHAR (40),
     age_group VARCHAR (100),
     race_ethnicity_combined VARCHAR(100),
     hosp_yn VARCHAR (40),
     icu_yn VARCHAR (40),
     death_yn VARCHAR (40),
-    medcond_yn (40)
+    medcond_yn VARCHAR (40)
 );
+
+
+-- Alter the Tables
+
+ALTER TABLE covid_19_data
+
+RENAME COLUMN "cdc_case_earliest_dt" TO "cdc_case_earliest_dt " ;
