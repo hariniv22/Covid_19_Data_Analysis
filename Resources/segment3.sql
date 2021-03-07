71ab35e221f6f5966139bfb04d4dcc18b0754fb2
hariniv22 <45175659+hariniv22@users.noreply.github.com>
2021-03-06 19:47:53 -0800

    Update segment3.sql

--- Resources/segment3.sql
+++ Resources/segment3.sql
@@ -1,23 +1,63 @@
--- Create table for COVID-19 Dataset
-
+-- Create a table for first dataset
 CREATE TABLE covid_19_data (
-    cdc_case_earliest_dt DATE IS NOT NULL,
-    cdc_report_dt DATE IS NOT NULL,
-    pos_spec_dt DATE IS NOT NULL,
-    onset_dt DATE IS NOT NULL,
-    current_status VARCHAR(40),
+    cdc_case_earliest_dt DATE NOT NULL,
+    cdc_report_dt DATE NOT NULL,
+    pos_spec_dt DATE NOT NULL,
+    onset_dt DATE NOT NULL,
+    current_status VARCHAR NOT NULL,
     sex VARCHAR (40),
     age_group VARCHAR (100),
     race_ethnicity_combined VARCHAR(100),
     hosp_yn VARCHAR (40),
     icu_yn VARCHAR (40),
     death_yn VARCHAR (40),
-    medcond_yn VARCHAR (40)
+    medcond_yn VARCHAR(40)
 );
 
-
--- Alter the Tables
-
+-- Alter column "cdc_case_earliest_dt" to match the dataset
 ALTER TABLE covid_19_data
-
 RENAME COLUMN "cdc_case_earliest_dt" TO "cdc_case_earliest_dt " ;
+
+SELECT * FROM covid_19_data;
+
+
+
+--DROP TABLE covid_19_data;
+
+CREATE TABLE covid_data (
+    cdc_case_earliest_dt DATE NOT NULL,
+    cdc_report_dt DATE NOT NULL,
+    pos_spec_dt DATE NOT NULL,
+    onset_dt DATE NOT NULL,
+    current_status VARCHAR NOT NULL,
+    sex VARCHAR (40),
+    age_group VARCHAR (100),
+    race_ethnicity_combined VARCHAR(100),
+    hosp_yn VARCHAR (40),
+    icu_yn VARCHAR (40),
+    death_yn VARCHAR (40),
+    medcond_yn VARCHAR(40),
+	PRIMARY KEY (race_ethnicity_combined) V
+);
+
+-- Create table for second dataset
+CREATE TABLE population (
+    race_ethnicity_combined VARCHAR(100),
+	population_distribution FLOAT(40)
+	--FOREIGN KEY(race_ethnicity_combined) REFERENCES covid_19_data( race_ethnicity_combined)
+);
+
+DROP TABLE population;
+
+-- Perform INNER JOIN on two tables and load the merged data into a new table(covid_merged_population)
+SELECT c.cdc_case_earliest_dt, c.cdc_report_dt, c.pos_spec_dt, c.onset_dt, c.current_status,
+		c.sex, c.age_group, c.race_ethnicity_combined,
+		c.hosp_yn,c.icu_yn, c.death_yn, c.medcond_yn, p.population_distribution
+INTO covid_merged_population
+FROM covid_19_data AS c
+INNER JOIN  population as p
+ON c.race_ethnicity_combined = p.race_ethnicity_combined
+ORDER BY c.race_ethnicity_combined
+
+
+
